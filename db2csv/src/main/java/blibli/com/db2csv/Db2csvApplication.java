be5374bde22689917a2d00c1c1dbcac94010286a
Nikolaus Aldo Halim <Nikolausaldoh@gmail.com>
2018-01-29 10:42:26 +0700

    init spring initializr

---
+++ db2csv/src/main/java/blibli/com/db2csv/Db2csvApplication.java
@@ -0,0 +1,12 @@
+package blibli.com.db2csv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Db2csvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Db2csvApplication.class, args);
+	}
+}
---
+++ db2csv/src/test/java/blibli/com/db2csv/Db2csvApplicationTests.java
@@ -0,0 +1,16 @@
+package blibli.com.db2csv;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Db2csvApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
